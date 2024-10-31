8d1179e0f28e62e89ac8bff1fae2c227ac7554be
Eduardo Roseo <eduardoroseo@gmail.com>
2024-10-31 19:37:26 -0300

    feat: exercicio 1 - javascript

---
+++ 3-javascript-exercicios/js/script1.js
@@ -0,0 +1,8 @@
+document.getElementById("btnCalcular").addEventListener('click', function() {
+    let numero1 = parseFloat(document.getElementById('numero1').value)
+    let numero2 = parseFloat(document.getElementById('numero2').value)
+
+    let resultado = numero1 + numero2
+
+    alert(resultado)
+})
