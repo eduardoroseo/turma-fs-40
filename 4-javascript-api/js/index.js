46c3382cbaf3f14d2fa3cff600a15573f3ce3c3f
Eduardo Roseo <eduardoroseo@gmail.com>
2024-11-28 22:00:34 -0300

    feat: update database.json and modify HTML structure for product listing

--- 4-javascript-api/js/index.js
+++ 4-javascript-api/js/index.js
@@ -1,16 +1,60 @@
-function consultarCEP() {
-  const id = document.getElementById('cep').value
+async function listarProdutos() {
+  try {
+    const response = 
+      await fetch("https://fakestoreapi.com/products")
 
-  fetch(`http://localhost:3000/alunos/${id}`)
-    .then((response) => response.json())
-    .then((endereco) => {
-      document.getElementById("resultado").innerHTML = 
-      `
-        <ul>
-          <li>ID: ${endereco.id}</li>
-          <li>Nome: ${endereco.nome}</li>
-          <li>Idade: ${endereco.idade}</li>
-        </ul>
+    const products = await response.json()
+
+    const productsHtml = products.map(product => {
+      return `
+        <tr>
+          <td>${product.id}</td>
+          <td>${product.title}</td>
+          <td>${product.price}</td>
+          <td>${product.category}</td>
+          <td>
+            <img 
+              src="${product.image}"
+              alt="${product.title}"
+              width="100px"
+              height="100px" />
+          </td>
+          <td>${product.rating.rate}</td>
+        </tr>
       `
     });
+
+    document.getElementById("lista-produtos").innerHTML = productsHtml.join("")
+
+  } catch (e) {
+    console.log(e.message)
+    console.log("deu ruim")
+    return
+  }
+  console.log("FIM DA FUNÇÃO")
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  listarProdutos()
+})
+
+
+
+
+
+
+
+
+// function listarProdutos() {
+//   fetch("https://fakestoreai.com/products")
+//     .then(response => response.json())
+//     .then(products => {
+//       // console.log(products)
+//       console.log("CARREGOU OS PRODUTOS")
+//     })
+//     .catch(() => {
+//       console.log("deu ruim")
+//     })
+
+//   console.log("FIM DA FUNÇÃO")
+// }
